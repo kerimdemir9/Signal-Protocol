27da082674c4b7bca2bbe07ba20ff0084151bc46
Kerim Demir <kerim.demir2002@hotmail.com>
2023-12-21 00:22:58 +0300

    phase 1 done

--- main.py
+++ main.py
@@ -11,19 +11,15 @@ import random
 import re
 import json
 import Crypto
-from Crypto.Hash import SHA3_256
+from Crypto.Hash import SHA3_256, HMAC, SHA256
 from Crypto import Random  # a bit better secure random number generation
 
 API_URL = 'http://harpoon1.sabanciuniv.edu:9999'
 
 stuID = 28853
-IK_Ser_Pub = {
-    "x": "1d42d0b0e55ccba0dd86df9f32f44c4efd7cbcdbbb7f36fd38b2ca680ab126e9",
-    "y": "ce091928fa3738dc18f529bf269ade830eeb78672244fd2bdfbadcb26c4894ff",
-    "x_dec": int("1d42d0b0e55ccba0dd86df9f32f44c4efd7cbcdbbb7f36fd38b2ca680ab126e9", 16),
-    "y_dec": int("ce091928fa3738dc18f529bf269ade830eeb78672244fd2bdfbadcb26c4894ff", 16)
-}
 curve = Curve.get_curve('secp256k1')
+IKey_Ser_Pub = Point(int("1d42d0b0e55ccba0dd86df9f32f44c4efd7cbcdbbb7f36fd38b2ca680ab126e9", 16),
+                     int("ce091928fa3738dc18f529bf269ade830eeb78672244fd2bdfbadcb26c4894ff", 16), curve)
 n = curve.order
 p = curve.field
 P = curve.generator
@@ -37,7 +33,7 @@ def IKRegReq(h,s,x,y):
     response = requests.put('{}/{}'.format(API_URL, "IKRegReq"), json = mes)
     print(response.json())
 
-def IKRegVerify(code):
+def IKRegVerify(ik_pri, ik_pub, code):
     mes = {'ID':stuID, 'CODE': code}
     print("Sending message is: ", mes)
     response = requests.put('{}/{}'.format(API_URL, "IKRegVerif"), json = mes)
@@ -45,7 +41,7 @@ def IKRegVerify(code):
     else:
         print(response.json())
         f = open('Identity_Key.txt', 'w')
-        f.write("IK.Prv: "+str(IKey_Pr)+"\n"+"IK.Pub.x: "+str(IKey_Pub.x)+"\n"+"IK.Pub.y: "+str(IKey_Pub.y))
+        f.write("IK.Prv: "+str(ik_pri)+"\n"+"IK.Pub.x: "+str(ik_pub[0])+"\n"+"IK.Pub.y: "+str(ik_pub[1]))
         f.close()
 
 def SPKReg(h,s,x,y):
@@ -135,15 +131,10 @@ def verify_signature(public_key, message, signature, curve):
     return h == h_prime
 
 
-def concatenate_integers_as_bytes(num1, num2):
-    num1_bytes_length = (num1.bit_length() + 7) // 8
-    num2_bytes_length = (num2.bit_length() + 7) // 8
-
-    num1_bytes = num1.to_bytes(num1_bytes_length, byteorder='big')
-    num2_bytes = num2.to_bytes(num2_bytes_length, byteorder='big')
-
-    concatenated_bytes = num1_bytes + num2_bytes
-
+def concatenate(x, y):
+    spk_x_bytes = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
+    spk_y_bytes = y.to_bytes((y.bit_length() + 7) // 8, byteorder='big')
+    concatenated_bytes = spk_x_bytes + spk_y_bytes
     return int.from_bytes(concatenated_bytes, byteorder='big')
 
 
@@ -156,5 +147,30 @@ IK_Pri = 14995333786290924431818846601387591473088369314019658467403653066049118
 verification_code = 630633
 reset_code = 645077
 # IKRegVerify(IK_Pri, IK_Pub, verification_code)
-SPK_Pub = 0
-SPK_Pri = 0
+SPK_Pub = (0x9907000b3b46c9308462dd70e0c0c2506cb562ff9ca25a916d2e67a68b5670e0 ,
+           0xed4930f2f4f7cb77c84c62526158b4d820af068af899ee3242a697a69408721c)
+SPK_Pri = 27280058814014322835872311304572730835600028459540571567859428260032877839542
+# SPKReg(SPK_h, SPK_s, SPK_Pub.x, SPK_Pub.y)
+T = SPK_Pri * IKey_Ser_Pub
+U = (b'TheHMACKeyToSuccess' + T.y.to_bytes((T.y.bit_length() + 7) // 8, byteorder='big')
+     + T.x.to_bytes((T.x.bit_length() + 7) // 8, byteorder='big'))
+
+KHMAC = SHA3_256.new(U)
+KHMAC = int(KHMAC.hexdigest(), 16)
+KHMAC_to_byte = KHMAC.to_bytes((KHMAC.bit_length()+7)//8, byteorder='big')
+
+OTKs = []
+HMACs = []
+
+for i in range(10):
+    OTK_Pub, OTK_Pri = keyGen()
+
+    OTK_Pub_x_to_byte = OTK_Pub.x.to_bytes((OTK_Pub.x.bit_length() + 7) // 8, byteorder='big')
+    OTK_Pub_y_to_byte = OTK_Pub.y.to_bytes((OTK_Pub.y.bit_length() + 7) // 8, byteorder='big')
+
+    hmac = HMAC.new(KHMAC_to_byte, OTK_Pub_x_to_byte + OTK_Pub_y_to_byte, digestmod=SHA256)
+    hmac = hmac.hexdigest()
+    HMACs.append(hmac)
+
+    OTKReg(i, OTK_Pub.x, OTK_Pub.y, hmac)
+
