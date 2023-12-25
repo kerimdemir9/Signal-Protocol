a0b806ac02590324ff2cf24f8869023a03bed4f3
Kerim Demir <kerim.demir2002@hotmail.com>
2023-12-25 12:35:17 +0300

    phase 2 update

---
+++ phase2_Client_basic.py
@@ -0,0 +1,160 @@
+import math
+import time
+import random
+import sympy
+import warnings
+from random import randint, seed
+import sys
+from ecpy.curves import Curve,Point
+from Crypto.Hash import SHA3_256, HMAC, SHA256
+import requests
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+import random
+import re
+import json
+
+API_URL = 'http://harpoon1.sabanciuniv.edu:9999/'
+
+stuID = # Enter your student ID
+stuIDB = 18007
+
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
+
+def Setup():
+    E = Curve.get_curve('secp256k1')
+    return E
+
+def KeyGen(E):
+    n = E.order
+    P = E.generator
+    sA = randint(1,n-1)
+    QA = sA*P
+    return sA, QA
+
+def SignGen(message, E, sA):
+    n = E.order
+    P = E.generator
+    k = randint(1, n-2)
+    R = k*P
+    r = R.x % n
+    h = int.from_bytes(SHA3_256.new(r.to_bytes((r.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    s = (sA*h + k) % n
+    return h, s
+
+def SignVer(message, h, s, E, QA):
+    n = E.order
+    P = E.generator
+    V = s*P - h*QA
+    v = V.x % n
+    h_ = int.from_bytes(SHA3_256.new(v.to_bytes((v.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    if h_ == h:
+        return True
+    else:
+        return False
+
+
+#server's Identitiy public key
+IKey_Ser = Point(13235124847535533099468356850397783155412919701096209585248805345836420638441, 93192522080143207888898588123297137412359674872998361245305696362578896786687, curve)
+
+def IKRegReq(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'IKPUB.X': x, 'IKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegReq"), json = mes)		
+    print(response.json())
+
+def IKRegVerify(code):
+    mes = {'ID':stuID, 'CODE': code}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegVerif"), json = mes)
+    if((response.ok) == False): raise Exception(response.json())
+    print(response.json())
+
+def SPKReg(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'SPKPUB.X': x, 'SPKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "SPKReg"), json = mes)		
+    print(response.json())
+
+def OTKReg(keyID,x,y,hmac):
+    mes = {'ID':stuID, 'KEYID': keyID, 'OTKI.X': x, 'OTKI.Y': y, 'HMACI': hmac}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "OTKReg"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+
+def ResetIK(rcode):
+    mes = {'ID':stuID, 'RCODE': rcode}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetIK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+def ResetSPK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetSPK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+def ResetOTK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetOTK"), json = mes)		
+    print(response.json())
+
+############## The new functions of phase 2 ###############
+
+#Pseudo-client will send you 5 messages to your inbox via server when you call this function
+def PseudoSendMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "PseudoSendMsg"), json = mes)		
+    print(response.json())
+
+#Get your messages. server will send 1 message from your inbox
+def ReqMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqMsg"), json = mes)	
+    print(response.json())	
+    if((response.ok) == True): 
+        res = response.json()
+        return res["IDB"], res["OTKID"], res["MSGID"], res["MSG"], res["IK.X"], res["IK.Y"], res["EK.X"], res["EK.Y"]
+
+#Get the list of the deleted messages' ids.
+def ReqDelMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqDelMsgs"), json = mes)      
+    print(response.json())      
+    if((response.ok) == True): 
+        res = response.json()
+        return res["MSGID"]
+
+#If you decrypted the message, send back the plaintext for checking
+def Checker(stuID, stuIDB, msgID, decmsg):
+    mes = {'IDA':stuID, 'IDB':stuIDB, 'MSGID': msgID, 'DECMSG': decmsg}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "Checker"), json = mes)		
+    print(response.json())
