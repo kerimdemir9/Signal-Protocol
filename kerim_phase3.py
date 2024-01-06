2848e33281bbfa891df9dfcb29a2b14d51f24588
Kerim Demir <kerim.demir2002@hotmail.com>
2024-01-06 12:22:01 +0300

    send message completed

--- kerim_phase3.py
+++ kerim_phase3.py
@@ -7,67 +7,6 @@ from Crypto.Hash import SHA3_256, HMAC, SHA256
 from Crypto import Random
 import Phase3.phase3_Client_basic as helper
 
-
-def sign_message(message, private_key):
-    k = Random.new().read(int(math.log(curve.order, 2)))
-    k = int.from_bytes(k, byteorder='big') % (curve.order - 1) + 1
-
-    R = k * P
-    r = R.x % n
-
-    r_to_byte = r.to_bytes((r.bit_length() + 7) // 8, byteorder='big')
-    message_to_byte = message.to_bytes((message.bit_length() + 7) // 8, byteorder='big')
-
-    hasher = SHA3_256.new().update(r_to_byte + message_to_byte)
-    h = int.from_bytes(hasher.digest(), byteorder='big') % n
-    s = (k - private_key * h) % curve.order
-
-    return (h, s)
-
-
-def concatenate(x, y):
-    x_to_bytes = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
-    y_to_bytes = y.to_bytes((y.bit_length() + 7) // 8, byteorder='big')
-    return x_to_bytes + y_to_bytes
-
-
-def generate_Ks_receiver(OTK_Priv_receiver, EK_Pub_sender, IK_Pub_sender, SPK_Pri_receiver, IK_Pri_receiver):
-    T1 = IK_Pub_sender * SPK_Pri_receiver
-    T2 = EK_Pub_sender * IK_Pri_receiver
-    T3 = EK_Pub_sender * SPK_Pri_receiver
-    T4 = EK_Pub_sender * OTK_Priv_receiver
-    U = concatenate(T1.x, T1.y) + concatenate(T2.x, T2.y) + concatenate(T3.x, T3.y) + concatenate(T4.x,
-                                                                                                  T4.y) + b'WhatsUpDoc'
-    Ks = int(SHA3_256.new(U).hexdigest(), 16)
-    return Ks
-
-
-def generate_Ks_sender(IK_Pub_b, Ek_Pri_sender, SPK_Pub_b, IK_Pri_a, OTK_Pub_b):
-    T1 = IK_Pub_b * Ek_Pri_sender
-    T2 = SPK_Pub_b * Ek_Pri_sender
-    T3 = OTK_Pub_b * Ek_Pri_sender
-    T4 = SPK_Pub_b * IK_Pri_a
-    U = concatenate(T1.x, T1.y) + concatenate(T2.x, T2.y) + concatenate(T3.x, T3.y) + concatenate(T4.x,
-                                                                                                  T4.y) + b'WhatsUpDoc'
-
-    return int(SHA3_256.new(U).hexdigest(), 16)
-
-
-def KDF_Chain(kdf):
-    k_enc = int(
-        SHA3_256.new(kdf.to_bytes((kdf.bit_length() + 7) // 8, byteorder='big') + b'JustKeepSwimming').hexdigest(), 16)
-    k_hmac = int(SHA3_256.new(concatenate(kdf, k_enc) + b'HakunaMatata').hexdigest(), 16)
-    kdf_next = int(SHA3_256.new(concatenate(k_enc, k_hmac) + b'OhanaMeansFamily').hexdigest(), 16)
-    return k_enc, k_hmac, kdf_next
-
-
-def keyGen():
-    secret = Random.new().read(int(math.log(n, 2)))
-    secret = int.from_bytes(secret, byteorder='big') % n
-    public = secret * P
-    return public, secret
-
-
 stuID_A = 28853
 stuID_B = 28928
 curve = Curve.get_curve('secp256k1')
@@ -148,35 +87,132 @@ OTKs = [
     },
 ]
 
-h, s = sign_message(stuID_B, IK_Pri)
 
-(OTK_ID_B, IK_Pub_B_x, IK_Pub_B_y, SPK_Pub_B_x, SPK_Pub_B_y,
- h_sign_pre_key, s_sign_pre_key, OTK_B_x, OTK_B_y) = helper.reqKeyBundle(stuID_A, stuID_B, h, s)
+def sign_message(message, private_key):
+    k = Random.new().read(int(math.log(curve.order, 2)))
+    k = int.from_bytes(k, byteorder='big') % (curve.order - 1) + 1
 
-if helper.SignVer(concatenate(SPK_Pub.x, SPK_Pub.y), h_sign_pre_key, s_sign_pre_key, curve, IK_Pub):
-    print("signature verified")
+    R = k * P
+    r = R.x % n
 
-    EK_Pub, EK_Priv = keyGen()
+    r_to_byte = r.to_bytes((r.bit_length() + 7) // 8, byteorder='big')
+    message_to_byte = message.to_bytes((message.bit_length() + 7) // 8, byteorder='big')
 
-    ks = generate_Ks_sender(Point(IK_Pub_B_x, IK_Pub_B_y, curve), EK_Priv,
-                            Point(SPK_Pub_B_x, SPK_Pub_B_y, curve), IK_Pri, Point(OTK_B_x, OTK_B_y, curve))
+    hasher = SHA3_256.new().update(r_to_byte + message_to_byte)
+    h = int.from_bytes(hasher.digest(), byteorder='big') % n
+    s = (k - private_key * h) % curve.order
 
-    for i in range(OTK_ID_B):
-        K_enc, K_HMAC, ks = KDF_Chain(ks)
+    return (h, s)
 
+
+def concatenate(x, y):
+    x_to_bytes = x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
+    y_to_bytes = y.to_bytes((y.bit_length() + 7) // 8, byteorder='big')
+    return x_to_bytes + y_to_bytes
+
+
+def generate_Ks_receiver(OTK_Priv_receiver, EK_Pub_sender, IK_Pub_sender, SPK_Pri_receiver, IK_Pri_receiver):
+    T1 = IK_Pub_sender * SPK_Pri_receiver
+    T2 = EK_Pub_sender * IK_Pri_receiver
+    T3 = EK_Pub_sender * SPK_Pri_receiver
+    T4 = EK_Pub_sender * OTK_Priv_receiver
+    U = concatenate(T1.x, T1.y) + concatenate(T2.x, T2.y) + concatenate(T3.x, T3.y) + concatenate(T4.x,
+                                                                                                  T4.y) + b'WhatsUpDoc'
+    Ks = int(SHA3_256.new(U).hexdigest(), 16)
+    return Ks
+
+
+def generate_Ks_sender(IK_Pub_b, Ek_Pri_sender, SPK_Pub_b, IK_Pri_sender, OTK_Pub_b):
+    T1 = IK_Pub_b * Ek_Pri_sender
+    T2 = SPK_Pub_b * Ek_Pri_sender
+    T3 = OTK_Pub_b * Ek_Pri_sender
+    T4 = SPK_Pub_b * IK_Pri_sender
+    U = concatenate(T1.x, T1.y) + concatenate(T2.x, T2.y) + concatenate(T3.x, T3.y) + concatenate(T4.x,
+                                                                                                  T4.y) + b'WhatsUpDoc'
+
+    return int(SHA3_256.new(U).hexdigest(), 16)  # Ks is the return value
+
+
+def KDF_Chain(kdf):
+    k_enc = int(
+        SHA3_256.new(kdf.to_bytes((kdf.bit_length() + 7) // 8, byteorder='big') + b'JustKeepSwimming').hexdigest(), 16)
+    k_hmac = int(SHA3_256.new(concatenate(kdf, k_enc) + b'HakunaMatata').hexdigest(), 16)
+    kdf_next = int(SHA3_256.new(concatenate(k_enc, k_hmac) + b'OhanaMeansFamily').hexdigest(), 16)
+    return k_enc, k_hmac, kdf_next
+
+
+def keyGen():
+    secret = Random.new().read(int(math.log(n, 2)))
+    secret = int.from_bytes(secret, byteorder='big') % n
+    public = secret * P
+    return public, secret
+
+
+def encrypt(k_hmac, k_enc, message):
+    # random 8 byte nonce
     nonce = random.randint(2 ** 63, 2 ** 64 - 1)
     nonce_to_byte = nonce.to_bytes((nonce.bit_length() + 7) // 8, byteorder='big')
 
-    aes = AES.new(K_enc.to_bytes((K_enc.bit_length() + 7) // 8, byteorder='big'), AES.MODE_CTR, nonce=nonce_to_byte)
+    # encryption algorithm
+    aes = AES.new(k_enc.to_bytes((k_enc.bit_length() + 7) // 8, byteorder='big'), AES.MODE_CTR, nonce=nonce_to_byte)
 
-    message = "HELLO!"
+    # cipherText
+    cipher_text = aes.encrypt(bytes(message.encode("utf-8")))
 
-    cipher_text = aes.encrypt(bytes(message.encode()))
-
-    H_MAC = HMAC.new(K_HMAC.to_bytes((K_HMAC.bit_length() + 7) // 8, byteorder="big"), msg=cipher_text,
+    H_MAC = HMAC.new(k_hmac.to_bytes((k_hmac.bit_length() + 7) // 8, byteorder="big"), msg=cipher_text,
                      digestmod=SHA256)
-
-    MAC = ((int.from_bytes(H_MAC.digest(), byteorder="big") % n)
+    # MAC value for our message
+    MAC = ((int.from_bytes(H_MAC.digest(), byteorder="big") % n)  # so that it is inside the curve
            .to_bytes(((int.from_bytes(H_MAC.digest(), byteorder="big") % n).bit_length() + 7) // 8, byteorder='big'))
+    # encrypted text
+    return int.from_bytes(nonce_to_byte + cipher_text + MAC, byteorder="big")
 
-    encrypted_message = int.from_bytes(nonce_to_byte + cipher_text + MAC, byteorder="big")
+
+def sendMessage(stuIDA, stuIDB, message, message_id):
+    h, s = sign_message(stuID_B, IK_Pri)
+
+    # get pre-key-bundle
+    (OTK_ID_B, IK_Pub_B_x, IK_Pub_B_y, SPK_Pub_B_x, SPK_Pub_B_y,
+     h_sign_pre_key, s_sign_pre_key, OTK_B_x, OTK_B_y) = helper.reqKeyBundle(stuID_A, stuID_B, h, s)
+
+    # check verification
+    if helper.SignVer(concatenate(SPK_Pub_B_x, SPK_Pub_B_y), h_sign_pre_key, s_sign_pre_key, curve,
+                      Point(IK_Pub_B_x, IK_Pub_B_y, curve)):
+        print("signature verified")
+        # generate EK
+        EK_Pub, EK_Priv = keyGen()
+
+        # generate Ks
+        ks = generate_Ks_sender(Point(IK_Pub_B_x, IK_Pub_B_y, curve), EK_Priv,
+                                Point(SPK_Pub_B_x, SPK_Pub_B_y, curve), IK_Pri, Point(OTK_B_x, OTK_B_y, curve))
+
+        for i in range(OTK_ID_B):
+            K_enc, K_HMAC, ks = KDF_Chain(ks)
+
+        cipher_text = encrypt(K_HMAC, K_enc, message)
+        helper.SendMsg(stuIDA, stuIDB, OTK_ID_B, message_id, cipher_text, IK_Pub.x, IK_Pub.y, EK_Pub.x, EK_Pub.y)
+    else:
+        print("signature not verified")
+
+
+def statusCheck():
+    h, s = sign_message(stuID_A, IK_Pri)
+    helper.Status(stuID_A, h, s)
+
+
+statusCheck()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
